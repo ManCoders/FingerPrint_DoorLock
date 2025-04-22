d28ace9460f6d9fe8d81cd1c38788960bfa07415
mattIwago <daligdig.manuel19@gmail.com>
2025-04-22 21:19:06 +0800

    Create FingerPrint_DoorLock.ino

---
+++ FingerPrint_DoorLock/FingerPrint_DoorLock.ino
@@ -0,0 +1,14 @@
+
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
