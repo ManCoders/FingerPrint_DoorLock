06815a852925dd6846f181cc0b8fb4ef85c46147
mattIwago <daligdig.manuel19@gmail.com>
2025-04-23 00:10:34 +0800

    2

--- FingerPrint_DoorLock/FingerPrint_DoorLock.ino
+++ FingerPrint_DoorLock/FingerPrint_DoorLock.ino
@@ -7,13 +7,16 @@ Adafruit_Fingerprint finger(&mySerial);
 const int relay = 4;
 const int ledRed = 11;
 const int ledBlue = 12;
+const int ledGreen = 10;
 const int vibrator = 6;
+
 #define buzzer 5
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledRed,OUTPUT);
-  pinMode(ledBlue,OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(relay, OUTPUT);
   while (!Serial)
@@ -25,15 +28,14 @@ void setup() {
 
   while (!finger.verifyPassword()) {
     Serial.println("Waiting for fingerprint sensor...");
+    ledAlert();
     delay(1000);  // Wait a bit before retrying
   }
 
   Serial.println("Found fingerprint sensor!");
-    finger.LEDcontrol(0x00);
-    digitalWrite(ledRed, HIGH);
-    enrollFingerprint();
-    
+  finger.LEDcontrol(0x00);
 
+  enrollFingerprint();
 }
 
 void loop() {
@@ -41,8 +43,8 @@ void loop() {
   delay(100);  // Short delay to prevent rapid polling
 }
 
-void enrollFingerprint() {
-  digitalWrite(ledRed, HIGH);
+bool enrollFingerprint() {
+ digitalWrite(ledRed, HIGH);
   Serial.println("Finger Registering..");
 
   // Clear the fingerprint database
@@ -54,6 +56,7 @@ void enrollFingerprint() {
   }
 
   finger.LEDcontrol(0x01);
+  ledGranted();
   Serial.println("Place your finger...");
 
   // First scan
@@ -61,7 +64,7 @@ void enrollFingerprint() {
     ;
   if (finger.image2Tz(1) != FINGERPRINT_OK) {
     Serial.println("[Failed] Convert First Image");
-    return;
+    return false;
   }
   Serial.println("[Success] First image captured.");
   delay(1000);
@@ -71,7 +74,7 @@ void enrollFingerprint() {
   delay(2000);
   finger.LEDcontrol(0x01);
   Serial.println("Place the same finger again...");
-  
+
 
   // Wait for finger removal then second scan
   while (finger.getImage() != FINGERPRINT_NOFINGER)
@@ -80,7 +83,7 @@ void enrollFingerprint() {
     ;
   if (finger.image2Tz(2) != FINGERPRINT_OK) {
     Serial.println("[Failed] Convert Second Image");
-    return;
+    return false;
   }
   Serial.println("[Success] Second image captured.");
 
@@ -95,11 +98,12 @@ void enrollFingerprint() {
   } else {
     Serial.println("[Failed] Create Model, Please Try Again!");
     enrollFingerprint();
-    return;
+    return false;
   }
 
   finger.LEDcontrol(0x00);
   delay(2000);
+  return true;
 }
 
 void getFingerprintID() {
@@ -120,22 +124,53 @@ void getFingerprintID() {
     Serial.print("Confidence: ");
     Serial.println(finger.confidence);
     finger.LEDcontrol(0x00);
-    digitalWrite(relay,HIGH);
-    playAlertTone();
+    digitalWrite(relay, HIGH);
+    playAccessTone();
+    ledGranted();
     delay(3000);
-    digitalWrite(relay,LOW);
+    digitalWrite(relay, LOW);
   } else {
+    
+    playAlertTone();
     Serial.println("Finger not recognized.");
   }
   finger.LEDcontrol(0x00);
 }
 
+void ledGranted() {
+  for (int i = 0; i < 3; i++) {  // Blink LED 3 times
+    digitalWrite(ledGreen, HIGH);
+    delay(200);  // LED on for 200ms
+    digitalWrite(ledGreen, LOW);
+    delay(200);  // LED off for 200ms
+  }
+}
+
+void ledAlert() {
+  for(int i = 1; i<3; i++){
+    digitalWrite(ledRed, HIGH);
+    delay(200);
+    digitalWrite(ledRed, LOW);
+    delay(200);
+  }
+}
+
+
+void playAccessTone() {
+  ledAlert();
+  tone(buzzer, 1500);  // Play 1.5kHz tone
+  delay(200);          // For 200 milliseconds
+  noTone(buzzer);      // Stop tone
+  delay(100);
+  tone(buzzer, 2000);  // Then a higher tone (like a "ding-ding")
+  delay(200);
+  noTone(buzzer);
+}
 
 void playAlertTone() {
+  ledAlert();
   for (int i = 0; i < 3; i++) {  // Beep 3 times
     tone(buzzer, 1000);          // 1kHz tone
-    digitalWrite(buzzer,HIGH);
-    
     delay(200);                  // Play for 200ms
     noTone(buzzer);
     delay(100);  // Wait between beeps
